6a84ba45545fc3f1f9c7bbeaf259fb8c70416cb2
Marc Von Asken <vonasken@gmail.com>
2016-12-02 14:37:19 -0500

    Adding materialize

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,15 +1,7 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-
-var $grid = $('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: 160
-});
-
-$grid.on( 'click', '.grid-item', function() {
-  // change size of item via class
-  $( this ).toggleClass('grid-item--gigante');
-  // trigger layout
-  $grid.masonry();
-});
+// $(document).ready(function(){
+//
+//
+// })
