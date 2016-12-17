4fb594ec401e42d0a9f64dcdc090df5cfc6ed567
Marc Von Asken <vonasken@gmail.com>
2016-12-17 07:46:16 -0500

    aWS Changes i progress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,18 @@
 Rails.application.configure do
 
+# AWS PAPERCLIP =====================================
+config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
+
+
+# Account Link ======================================
   Plaid.config do |p|
     p.client_id = ENV['plaid_client_id']
     p.secret = ENV['plaid_secret']
